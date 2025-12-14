9beb05931c01791da7f5c185a9ec39a98bbb2a5c
Mafupa <ryankarera@gmail.com>
2025-12-14 02:03:09 +0200

    Added docker configuration

--- BAMS-backend/Program.cs
+++ BAMS-backend/Program.cs
@@ -56,6 +56,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// Apply migrations automatically on startup
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApiContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
